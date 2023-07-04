971d7e4415f36aba8db3c3848de178d1fa0685e7
FairyScript <h@queb.fun>
2023-07-04 16:47:18 +0800

    refac: raidTime

--- src/view/Raid.tsx
+++ src/view/Raid.tsx
@@ -1,4 +1,5 @@
 import { useStore } from '@/store/rootStore'
+import { getNextRaid, raidIsOpen } from '@/utils/raidTime'
 import {
   Box,
   Card,
@@ -12,35 +13,10 @@ import dayjs from 'dayjs'
 import type { Dayjs } from 'dayjs'
 import { memo } from 'react'
 
-/**
- *  月曜日 ～ 金曜日	
- *    1回目　14:00 ～ 15:00
-      2回目　18:00 ～ 19:00
-      3回目　22:00 ～ 23:00
-    土曜日 ～ 日曜日	
-      1回目　08:00 ～ 09:00
-      2回目　12:00 ～ 13:00
-      3回目　16:00 ～ 17:00
-      4回目　20:00 ～ 21:00
-      5回目　翌01:00 ～ 翌02:00 
- */
-
-const table = [
-  [1, 8, 12, 16, 20],
-  [1, 14, 18, 22],
-  [14, 18, 22],
-  [14, 18, 22],
-  [14, 18, 22],
-  [14, 18, 22],
-  [8, 12, 16, 20],
-]
-const dayCount = table.map(h => h.length)
-const flatTable = table.flat()
-
 const Raid: React.FC = () => {
   const { current } = useStore()
   const now = dayjs(current).tz('Asia/Tokyo')
-  const open = isOpen(now)
+  const open = raidIsOpen(now)
 
   return (
     <Card padding={5}>
@@ -101,52 +77,4 @@ const FutureRaid: React.FC<RaidItemProps> = ({ now }) => {
       ))}
     </div>
   )
-}
-
-function getNextRaid(current: Dayjs, count: number) {
-  const now = dayjs(current).tz('Asia/Tokyo')
-  const day = now.day()
-  const hour = now.hour()
-
-  const result: Dayjs[] = []
-
-  //find now in table
-  const preDayCount = dayCount.slice(0, day).reduce((a, b) => a + b, 0)
-  const hourIndex = table[day].findIndex(h => h > hour)
-  const index = preDayCount + (hourIndex !== -1 ? hourIndex : dayCount[day])
-  const nextHours = getCircularValues(flatTable, index, count)
-
-  let next = now
-  for (const hour of nextHours) {
-    next = getNextTime(next, hour)
-    result.push(next)
-  }
-
-  return result
-}
-
-function isOpen(current: Dayjs) {
-  return table[current.day()].includes(current.hour())
-}
-
-function getCircularValues<T = any>(
-  array: T[],
-  index: number,
-  count: number
-): T[] {
-  const len = array.length
-  const result = []
-  for (let i = 0; i < count; i++) {
-    result.push(array[(index + i) % len])
-  }
-  return result
-}
-
-function getNextTime(current: Dayjs, hour: number) {
-  const now = current.tz('Asia/Tokyo')
-  const next = now.hour(hour).startOf('hour')
-  if (now.isAfter(next)) {
-    return next.add(1, 'day')
-  }
-  return next
 }
